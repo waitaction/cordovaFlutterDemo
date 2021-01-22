c47121b335b1211b89b4aa298ccd986d6c89a20d
QiJing Chen <qijing@wo.cn>
2021-01-22 15:48:27 +0800

    test

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # cordovaFlutterDemo
 cordova 与 flutter 混合开发 的示例应用
+
+这个示例还有些问题，待完善
+
+运行ionic cordova run android --device
--- src/app/core/flutter.service.ts
+++ src/app/core/flutter.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 declare var flutter: any;
 declare var bridgeFlutter: any;
+declare var navigator: any;
+declare var cordova: any;
 @Injectable({
     providedIn: 'root'
 })
@@ -16,13 +18,7 @@ export class FlutterService {
         }, false);
     }
 
-    defFlutter() {
-        bridgeFlutter.getDate = function (jsonObj, callback) {
-            var format = jsonObj.format;
-            callback({ date: "日期格式是：" + format });
-            alert("日期格式是：" + format);
-        }
-    }
+
 
     open() {
         flutter.open(function () {
@@ -30,5 +26,44 @@ export class FlutterService {
         }, function (err) {
 
         });
+    }
+
+    /**
+     * 拍一张照片或者选择一张照片，返回照片的base64编码
+     */
+    takePhoto(sourceType: number = 0): Promise<string> {
+        let promise = new Promise<string>((resolve, reject) => {
+            navigator.camera.getPicture(function (imageURI) {
+                let base64 = "data:image/jpeg;base64," + imageURI;
+                resolve(base64);
+            }, function (message) {
+                reject(message);
+            }, {
+                quality: 50,
+                destinationType: 0,// Camera.DestinationType.DATA_URL,
+                mediaType: 0, // Camera.MediaType.PICTURE,
+                sourceType: sourceType // Camera.PictureSourceType.SAVEDPHOTOALBUM
+            });
+        });
+        return promise;
+    }
+
+    openInappbrowser() {
+        cordova.InAppBrowser.open('https://www.baidu.com', '_blank', 'location=yes');
+    }
+
+
+    /**
+     * 定义与flutter的桥接
+     */
+    defFlutter() {
+        let self = this;
+        bridgeFlutter.takePhoto = function (param, callback) {
+            self.takePhoto(param.sourceType).then(base64 => callback({ value: base64 }));
+        }
+        bridgeFlutter.openInappbrowser = function (param, callback) {
+            self.openInappbrowser();
+            callback({ result: true });
+        }
     }
 }
